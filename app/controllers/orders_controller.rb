1d8e402ebfc88aebf24ba937c060c74cf61cd073
bps-yamaguchi <46920892+bps-yamaguchi@users.noreply.github.com>
2023-10-07 14:39:37 +0900

    ポイント付与実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,36 +1,19 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :cancel]
+  before_action :authenticate_user!, only: [:checkout, :cancel]
 
-  def create
-    # ユーザーのカート情報を取得
-    cart_items = current_user.cart.items
+  def checkout
+    @order = Order.checkout_from_cart(current_user.cart)
 
-    # カートが空でないかチェック
-    if cart_items.empty?
-      redirect_to cart_path, alert: 'Your cart is empty. Please add items before checkout.'
-      return
-    end
-
-    # OrderとOrderDetailを作成
-    order = current_user.orders.build
-    cart_items.each do |item|
-      order.order_details.build(item: item, amount: item.amount)
-    end
-
-    # 保存とカートのクリア
-    if order.save
-      current_user.cart.items.clear
-      redirect_to order_path(order), notice: 'Your order has been placed successfully.'
+    if @order.persisted?
+      redirect_to @order, notice: 'Order was successfully created.'
     else
-      redirect_to cart_path, alert: 'There was a problem placing your order. Please try again.'
+      render :new
     end
   end
 
   def cancel
-    # 指定されたIDのOrderを取得
     order = current_user.orders.find(params[:id])
 
-    # Orderをキャンセル（実際のキャンセルロジックはモデル内で定義）
     if order.cancel
       redirect_to order_path(order), notice: 'Your order has been cancelled successfully.'
     else
@@ -40,7 +23,6 @@ class OrdersController < ApplicationController
 
   private
 
-  # ユーザーがログインしているかをチェック
   def authenticate_user!
     redirect_to login_path, alert: 'Please login to continue.' unless current_user
   end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,32 @@
 class Order < ApplicationRecord
   belongs_to :user
+  has_many :order_details, dependent: :destroy
+  has_many :items, through: :order_details
+
+  enum status: { pending: 0, shipped: 1, cancelled: 2 }
+
+  validates :user, presence: true
+  validates_associated :order_details
+
+  def self.checkout_from_cart(cart)
+    order = new(user: cart.user, status: :pending)
+
+    ActiveRecord::Base.transaction do
+      cart.items.each do |item|
+        order.order_details.build(item: item, amount: item.amount)
+      end
+
+      order.save!
+
+      PointLog.create!(user: order.user, points: order.points_to_add, trigger: order)
+    end
+
+    order
+  rescue ActiveRecord::RecordInvalid
+    order
+  end
+
+  def cancel
+    update(status: :cancelled)
+  end
 end
